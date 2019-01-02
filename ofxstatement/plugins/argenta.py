ba39dc55bde6683c6088913ade02c5cc7713bc3c
woutbr <woutbr@student.hik.be>
2019-01-02 23:45:51 +0100

    Added new trntype: 'Interestberekening'

--- ofxstatement/plugins/argenta.py
+++ ofxstatement/plugins/argenta.py
@@ -25,7 +25,8 @@ class ArgentaStatementParser(StatementParser):
         'SEPA-domiciliëring': 'DIRECTDEBIT',
         'Debet ten voordele van BCC': 'DEBIT',
         'Storting kaarttransactie': 'XFER',
-        'Opname Bancontact': 'ATM'
+        'Opname Bancontact': 'ATM',
+        'Interestberekening': 'INT'
     }# TODO Add more transaction types as I encounter them.
     # Transaction types used in <TRNTYPE>: xml.coverpages.org/OFEXFIN2.html#_Ref377532222
 
